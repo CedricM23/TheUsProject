8bef9958c86217a870df32b172b226dcfe9aa105
cedricm23 <Cedricpbc@gmail.com>
2026-09-12 12:41:59 -0400

    added more test objects until backend is working

--- React/src/services/DatesService.js
+++ React/src/services/DatesService.js
@@ -72,5 +72,20 @@ const dates = [
             'Phasellus gravida semper nisi. Nullam vel sem. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Sed hendrerit. Morbi ac felis.'
         ],
         scrapbookImageCaption: 'We ate so much pasta we couldn\'t walk.'
+    },
+     {
+        id: 5,
+        name: 'Moon Thai',
+        location: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3564.9448239270328!2d-80.22804948851044!3d26.682247076688412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d92f0f5a2af889%3A0xf6a9ffa6148838ec!2sMoon%20Thai%20%26%20Japanese!5e0!3m2!1sen!2sus!4v1748902194032!5m2!1sen!2sus',
+        imageOfPlace: 'https://placehold.co/600x400/ffb6c1/ffffff?text=Moon+Thai',
+        song: "https://embed.music.apple.com/us/album/when-we-were-friends/1517139758?i=1517139760",
+        articleTitle: 'Our First Date',
+        datetime: 'October 15th 2025',
+        Description: [
+            'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.',
+            'Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+            'Cras id est sit amet eros efficitur aliquam. Nullam in diam at mi rhoncus accumsan. Etiam dapibus, est sit amet pellentesque auctor, ante arcu finibus lectus, vitae sagittis ex ex elementum sem. Fusce vitae massa sit amet arcu aliquet scelerisque.'
+        ],
+        scrapbookImageCaption: 'Nervous smiles before the appetizers arrived!'
     }
 ]
