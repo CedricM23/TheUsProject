8a269725e63c3a4a1d9bc4e4d1d724ec6ba84ba7
cedricm23 <Cedricpbc@gmail.com>
2026-09-12 12:36:48 -0400

    Removed Dead space

--- React/src/components/DateCard/DateCard.jsx
+++ React/src/components/DateCard/DateCard.jsx
@@ -9,10 +9,8 @@ export default function DateCard({ date }) {
                 <img
                     src={date.imageOfPlace}
                     alt={date.name}
-                    /* Reduced md height to 275px, added w-full and object-cover to prevent stretching */
                     className="w-full object-cover rounded-2xl md:h-[275px] max-md:rounded-2xl"
                 />
-                {/* Changed text-base to text-sm, added margin-top, removed conflicting 'text-right' */}
                 <h3 className="text-center text-sm mt-2 text-[#C11C84]">
                     {date.name}
                     <FontAwesomeIcon
