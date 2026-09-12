27a81c60e4b9196132fd0956f3d406763b4eb240
cedricm23 <Cedricpbc@gmail.com>
2026-09-12 12:42:13 -0400

    Tags for MediaDetailView

---
+++ React/src/components/MetaTag/MetaTag.jsx
@@ -0,0 +1,7 @@
+export default function MetaTag({value}) {
+    return (
+        <span className="bg-zinc-900/60 backdrop-blur-md px-3 py-1 rounded-md border border-zinc-700">
+            {value}
+        </span>
+    )
+}
