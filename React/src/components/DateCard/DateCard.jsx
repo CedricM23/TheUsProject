fd93d40825fe6636c9f73cceea76f6b298032c32
cedricm23 <Cedricpbc@gmail.com>
2026-09-12 12:36:35 -0400

    removed dead space

--- React/src/components/DateCard/DateCard.jsx
+++ React/src/components/DateCard/DateCard.jsx
@@ -4,18 +4,20 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight'
 
 export default function DateCard({ date }) {
     return (
-        <article className="w-[400px] mb-2.5 rounded-2xl md:w-[600px] md:rounded-none">
-            <Link to={`/dates/${date.id}`} className="no-underline">
+        <article className="w-64 mb-2.5 rounded-2xl md:w-[450px] md:rounded-none">
+            <Link to={`/dates/${date.id}`} className="no-underline block">
                 <img
                     src={date.imageOfPlace}
                     alt={date.name}
-                    className="rounded-2xl md:h-[400px] max-md:rounded-2xl"
+                    /* Reduced md height to 275px, added w-full and object-cover to prevent stretching */
+                    className="w-full object-cover rounded-2xl md:h-[275px] max-md:rounded-2xl"
                 />
-                <h3 className="text-center text-base text-[#C11C84] text-right">
+                {/* Changed text-base to text-sm, added margin-top, removed conflicting 'text-right' */}
+                <h3 className="text-center text-sm mt-2 text-[#C11C84]">
                     {date.name}
                     <FontAwesomeIcon
                         icon={faArrowRight}
-                        className="ml-2.5 text-sm"
+                        className="ml-2 text-xs"
                     />
                 </h3>
             </Link>
