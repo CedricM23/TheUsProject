d8f66d076ba82a9f02efc8bee649d675d845db8a
cedricm23 <Cedricpbc@gmail.com>
2026-09-25 09:33:47 -0400

    dupluicate removal

--- Backend/database copy/data.sql
+++
@@ -1,7 +0,0 @@
-BEGIN TRANSACTION;
-
--- the password for both users is "password"
-INSERT INTO users (username,password_hash,first_name, last_name, role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','Testuser','Testuser','ROLE_USER');
-INSERT INTO users (username,password_hash,first_name, last_name, role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','Admin','Admin','ROLE_ADMIN');
-
-COMMIT TRANSACTION;
