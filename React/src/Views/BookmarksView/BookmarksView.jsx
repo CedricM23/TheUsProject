1eaafc28a5b874bd11fc29b66a5056c8501fd66f
cedricm23 <Cedricpbc@gmail.com>
2026-09-25 09:41:47 -0400

    Added bookmarks View to see Bookmarked Items

---
+++ React/src/Views/BookmarksView/BookmarksView.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import ShowService from "../../services/ShowService";
+import MediaCard from "../../components/MediaCard/MediaCard";
+import BookmarkService from "../../services/BookmarkService";
+
+export default function BookmarksView() {
+    const [favorites, setFavorites] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        BookmarkService.getMyBookmarks()
+            .then(response => {
+                const favoriteRecords = response.data; 
+                const tmdbPromises = favoriteRecords.map(record => {
+                    return ShowService.getDetails(record.mediaType, record.tmdbMediaId)
+                        .then(tmdbResponse => {
+                            return { 
+                                ...tmdbResponse.data, 
+                                media_type: record.mediaType 
+                            };
+                        });
+                });
+
+                return Promise.all(tmdbPromises);
+            })
+            .then(fullMediaData => {
+                setFavorites(fullMediaData);
+            })
+            .catch(error => {
+                console.error("Error loading favorites:", error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, []);
+
+    return (
+        <div className="container mx-auto p-4 text-center">
+            <h1 className="text-3xl font-bold mb-6">My Watchlist</h1>
+            
+            {isLoading ? (
+                <div className="flex justify-center mt-10">
+                    <span className="loading loading-spinner loading-lg"></span>
+                </div>
+            ) : favorites.length === 0 ? (
+                <p>You haven't added anything to you bookmarks yet!</p>
+            ) : (
+                <div className="flex flex-wrap gap-4 justify-center">
+                    {favorites.map(media => (
+                        <MediaCard key={media.id} movie={media} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
